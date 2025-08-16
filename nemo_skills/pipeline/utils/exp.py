e18b8cf12fe4a281979b77d9c69a13cfb234d074
SeanNaren <snarenthiran@nvidia.com>
2025-08-16 04:13:09 -0700

    allow overlapping sandbox with run_cmd
    Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

--- nemo_skills/pipeline/utils/exp.py
+++ nemo_skills/pipeline/utils/exp.py
@@ -187,7 +187,6 @@ def get_executor(
             ipc_mode="host",
             volumes=mounts,
             ntasks_per_node=1,
-            privileged=bool(os.getenv('NEMO_SKILLS_PRIVILEGED_DOCKER', 0)),
             # locally we are always asking for all GPUs to be able to select a subset with CUDA_VISIBLE_DEVICES
             num_gpus=-1 if gpus_per_node is not None else None,
             network="host",
@@ -471,7 +470,7 @@ def add_task(
                         heterogeneous=heterogeneous,
                         het_group=het_group,
                         total_het_groups=total_het_groups,
-                        overlap=server_config is not None,
+                        overlap=(server_config is not None) or with_sandbox,
                         with_ray=with_ray,
                     )
                 )
@@ -509,7 +508,7 @@ def add_task(
                 heterogeneous=heterogeneous,
                 het_group=het_group,
                 total_het_groups=total_het_groups,
-                overlap=server_config is not None,
+                overlap=True,
                 with_ray=with_ray,
             )
             executors.append(sandbox_executor)
@@ -643,5 +642,6 @@ def get_nsight_cmd(profile_step_range):
             f'export LD_LIBRARY_PATH="/usr/local/cuda/lib64:/usr/local/cuda/lib:/usr/local/nvidia/lib64:/usr/local/nvidia/lib:/usr/lib/x86_64-linux-gnu" && '
             f"export NRL_NSYS_PROFILE_STEP_RANGE={profile_step_range} && "
             'export NRL_NSYS_WORKER_PATTERNS="*policy*,*vllm*" && '
-        )
+
+            )
     return cmd
