ffca93981839239bd2e0eb6182b68553856a9bdf
SeanNaren <snarenthiran@nvidia.com>
2026-03-20 08:54:50 -0700

    update
    Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

--- scripts/singleagent/self-correct/memory_all_icpc25_claude_opus.py
+++ scripts/singleagent/self-correct/memory_all_icpc25_claude_opus.py
@@ -7,34 +7,34 @@ os.environ["NVIDIA_API_KEY"] = "sk-fX8ok2ChQOgI1SNqekeY3Q"
 
 model_name = "azure/anthropic/claude-opus-4-6"
 dataset = "icpc25"
-solutions = 32
+solutions = 24
 steps = 10
 num_runs = 1
 start_run = 0
 
 # Base directory for all runs
-output_base = f"/workspace/baselines/icpc/{model_name.split('/')[-1]}_self_correct_with_memory_{solutions}_2_solutions_all_{steps}_steps_sample_tests"
+output_base = f"/workspace/baselines/self-correct/icpc25/{model_name.split('/')[-1]}_self_correct_with_memory_{solutions}_2_solutions_all_{steps}_steps_pt2"
 # Directory containing evaluation datasets
 data_dir = "/workspace/data/eval_datasets/"
 
-cluster = "dfw"
+cluster = "cw-dfw"
 test_file = f"{data_dir}/{dataset}/test_metadata.json"
 
 for i in range(start_run, start_run + num_runs):
     print(f"Running {i}th experiment")
-    run_output_dir = f"{output_base}/runs/{i+1}"
+    run_output_dir = f"{output_base}/runs/{i + 1}"
     eval(
         ctx=wrap_arguments(
             "++skip_filled=True "
             "++prompt_config=generic/default "
             "++inference.endpoint_type=chat "
             "++inference.tokens_to_generate=128000 "
-            "++inference.temperature=1.0 "
-            "++inference.top_p=0.95 "
             "++inference.top_k=-1 "
+            "++inference.temperature=0.0 "
             "++time_limit=03:50:00 "
             f"++total_steps={steps} "
-            "++max_concurrent_requests=3 "
+            "++max_concurrent_requests=2 "
+            "++inference.reasoning_effort=max "
             "++show_k_solutions=2 "
             "++only_sample_tests=True "
             "++per_step_evaluate=True "
@@ -48,7 +48,6 @@ for i in range(start_run, start_run + num_runs):
         server_type="anthropic",
         server_address="https://inference-api.nvidia.com",
         single_node_mode=SingleNodeMode.sequential,
-        num_jobs=10,
         dependent_jobs=10,
         benchmarks=f"{dataset}:{solutions}",
         data_dir=data_dir,
